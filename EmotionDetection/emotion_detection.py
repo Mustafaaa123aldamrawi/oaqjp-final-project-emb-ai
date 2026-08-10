63826cb41e9053e2f03b810c5f83d664bd69acfe
Mustafa Aldamrawi <mustafa@maktabitech.com>
2026-08-10 17:08:38 -0400

    Complete final Emotion Detection project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,6 +3,16 @@ import json
 
 
 def emotion_detector(text_to_analyze):
+    if not text_to_analyze or not text_to_analyze.strip():
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+
     url = (
         "https://sn-watson-emotion.labs.skills.network/"
         "v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -21,8 +31,17 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json=input_json, headers=headers)
 
-    formatted_response = json.loads(response.text)
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
 
+    formatted_response = json.loads(response.text)
     emotions = formatted_response["emotionPredictions"][0]["emotion"]
 
     anger_score = emotions["anger"]
